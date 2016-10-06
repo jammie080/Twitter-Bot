8e0a34781de4fde0c2d5abaf57ca287b72061e00
jammie messam <diycertified@aol.com>
2016-10-06 01:28:09 -0400

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='Twitter=Bot',
-      version='1.0.5.2',
+      version='1.0.5.3',
 
       description='Just another twitter bot',
 
--- twitter/version.py
+++ twitter/version.py
@@ -1,4 +1,4 @@
-__version__ = "v1.0.5.2"
+__version__ = "v1.0.5.3"
 __author__ = "Jammie Messam"
 __email__ = "diycertified@aol.com"
 __date__ = ""
