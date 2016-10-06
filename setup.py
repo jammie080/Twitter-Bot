c62913e100fceedb6903a100a10b0b79bc2e66a9
jammie messam <diycertified@aol.com>
2016-10-05 21:44:19 -0400

    Setting up

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 def read(fname):
@@ -6,11 +6,43 @@ def read(fname):
 
 setup(name='Twitter=Bot',
       version='1.0.5',
+
       description='Just another twitter bot',
-      url='https://github.com/jammie080/Twitter-Bott',
+
+      url='https://github.com/jammie080/Twitter-Bot',
+
       author='Jammie Messam',
+
       author_email='diycertified@aol.com',
-      license='MIT',
-      packages=['Twitter-Bot'],
-      long_description=read('README'),
+
+      license=read('LICENSE'),
+
+      classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7'
+
+    ],
+
+      keywords='twitter bot development',
+
+      packages=find_packages(exclude=('examples', 'settings','twitter')),
+
+      install_requires=['requirements.txt'],
+
+      long_description=read('README.md'),
+
       zip_safe=False)
